11f0f128c29a583d88c8d987070a4f52ff80b4b6
Indra Prahasta <4705180+ip10111@users.noreply.github.com>
2023-05-23 19:55:57 +0700

    Update is_prime_number.rs

--- src/is_prime_number.rs
+++ src/is_prime_number.rs
@@ -11,12 +11,44 @@ fn main() {
     println!("{}", info);
 }
 
+// Learning version.
 fn is_prime(x: i32) -> bool {
     if x < 2 {
         return false;
     }
 
     for i in 2..x - 1 {
+        if x % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+// Optimized version.
+// set x to u32, since negative numbers are not considered prime.
+fn is_prime_opt(x: u32) -> bool {
+    if x < 2 {
+        return false;
+    }
+
+    // Special case.
+    if x == 2 {
+        return true;
+    }
+
+    // Handling all even number.
+    if x % 2 == 0 {
+        return false;
+    }
+
+    // Set the limit as the square root of x.
+    // because if x is not prime and has a divisor greater than its square root, then it must also have a corresponding divisor smaller than its square root.
+    let limit = (x as f64).sqrt() as u32;
+
+    // Looping while skip all even number.
+    for i in (3..=limit).step_by(2) {
         if x % i == 0 {
             return false;
         }
