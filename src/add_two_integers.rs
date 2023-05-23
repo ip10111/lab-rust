2c436c46b2a45832405efdd8531ace2c765fec4a
Indra Prahasta <4705180+ip10111@users.noreply.github.com>
2023-05-23 09:00:14 +0700

    Create add_two_integers.rs

---
+++ src/add_two_integers.rs
@@ -0,0 +1,15 @@
+// Add two integers and print the result.
+
+fn main() {
+
+    let a = 5;
+    let b = 7;
+    
+    let sum = add(a, b);
+    
+    println!("{a} + {b} = {}", sum);
+}
+
+fn add(x:i32, y:i32) -> i32 {
+    return x + y;
+}
